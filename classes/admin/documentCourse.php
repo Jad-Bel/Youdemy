1d6630c37dbfdab4ba608f92b4c334ea635f8a64
Jad-Bel <jadthegamer06@gmail.com>
2025-01-14 17:27:20 +0100

    Enhance DocumentCourse class to manage document paths and implement download functionality

--- classes/admin/documentCourse.php
+++ classes/admin/documentCourse.php
@@ -1,17 +1,35 @@
-<?php 
+<?php
+require_once 'Course.php';
 
 class DocumentCourse extends Course {
-    private $docuemnt_path;
+    private $document_path;
 
-    public function __construct() {
-
+    public function __construct($title, $description, $teacher_id, $category_id, $document_path) {
+        parent::__construct($title, $description, $teacher_id, $category_id);
+        $this->document_path = $document_path;
     }
 
-    public function getDocumentPath() {}
-    public function setDocumentPath($document_path) {}
+    public function getDocumentPath() {
+        return $this->document_path;
+    }
 
-    public function displayContent() {}
+    public function setDocumentPath($document_path) {
+        $this->document_path = $document_path;
+    }
 
-    public function save() {}
+    public function displayContent() {
+        return "<a href='{$this->document_path}' download>Download Document</a>";
+    }
 
+    public function save() {
+        parent::save();
+
+        $sql = "UPDATE courses SET content = :content WHERE id = :id";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([
+            'content' => $this->document_path,
+            'id' => $this->id
+        ]);
+    }
 }
+?>
