eff10b8dc37a61a2dc4d315415399b38c945c8ff
Jad-Bel <jadthegamer06@gmail.com>
2025-01-14 16:22:26 +0100

    Refactor Course class to include created_at and updated_at properties, and add getter/setter methods for title, description, teacher_id, category_id, and created_id

--- classes/admin/course.php
+++ classes/admin/course.php
@@ -7,19 +7,60 @@ abstract class course
     protected $description;
     protected $teacher_id;
     protected $category_id;
-    protected $created_id;
+    protected $created_at;
+    protected $updated_at;
+    protected $db;
 
-    public function __construct($title, $description, $teacher_id, $category_id)
+    public function __construct()
     {
-        
+        $this->db = new database();
     }
 
     public function setId ($id) {
-        return $this->id = $id;
+        $this->id = $id;
     }
 
     public function getId () {
-        $this->id;
+        return $this->id;
     }
 
+    public function settitle ($title) {
+        $this->title = $title;
+    }
+
+    public function gettitle () {
+        return $this->title;
+    }
+
+    public function setdescription ($description) {
+        $this->description = $description;
+    }
+
+    public function getdescription () {
+        return $this->description;
+    }
+
+    public function setteacher ($teacher_id) {
+        $this->teacher_id = $teacher_id;
+    }
+
+    public function getteacher () {
+        return $this->teacher_id;
+    }
+
+    public function setcategory ($category_id) {
+        $this->category_id = $category_id;
+    }
+
+    public function getcategory () {
+        return $this->category_id;
+    }
+
+    public function setcreated ($created_id) {
+        $this->created_id = $created_id;
+    }
+
+    public function getcreated () {
+        return $this->created_id;
+    }
 }
