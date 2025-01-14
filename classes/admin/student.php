0b06c93b96cd159a4012e44a3d8ff7bec7a7e37f
Jad-Bel <jadthegamer06@gmail.com>
2025-01-14 15:16:41 +0100

    Add user, student, teacher, and auth classes with basic methods

---
+++ classes/admin/student.php
@@ -0,0 +1,17 @@
+<?php 
+
+require_once '../user.php';
+
+
+class student extends user 
+{
+    public function enroll($course_id)
+    {
+
+    }
+
+    public function viewCourses() 
+    {
+        
+    }
+}
