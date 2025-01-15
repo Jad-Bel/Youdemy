e6c2aec402dba8eb0537da950d781bc1b0f5f27f
Jad-Bel <jadthegamer06@gmail.com>
2025-01-15 23:34:48 +0100

    REFAC:Update database configuration with placeholder values and improve exception handling in the Database class

--- config/database.php
+++ config/database.php
@@ -1,21 +1,20 @@
-<?php 
-
+<?php
 class Database {
     private $host = "localhost";
-    private $db_name = "driveLocV2";
-    private $username = "root";
-    private $password = "Hitler20.";
+    private $db_name = "your_database_name";
+    private $username = "your_username";
+    private $password = "your_password";
     private $conn;
-
 
     public function getConnection() {
         $this->conn = null;
         try {
             $this->conn = new PDO("mysql:host={$this->host};dbname={$this->db_name}", $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (Exception $e) {
-            throw new Error("Connection error: " . $e->getMessage());
+        } catch (PDOException $e) {
+            throw new Exception("Connection error: " . $e->getMessage());
         }
         return $this->conn;
     }
 }
+?>
