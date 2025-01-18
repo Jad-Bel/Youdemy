a2fd9e85c1b0d3d211c8b1c73431ac72122da080
Jad-Bel <jadthegamer06@gmail.com>
2025-01-18 21:22:31 +0100

    REFAC: Update course details display to use dynamic content for description, duration, skill level, language, and certification

--- pages/course_details.php
+++ pages/course_details.php
@@ -344,7 +344,7 @@ $course = $courseService->getCourseById($courseId);
 											<h2 class="post-title"><?= $course['title'] ?></h2>
 										</div>
 										<div class="ttr-post-text">
-											<p><?= $course['dsc'] ?></p>
+											<p><?= $course['content'] ?></p>
 										</div>
 									</div>
 								</div>
@@ -353,126 +353,23 @@ $course = $courseService->getCourseById($courseId);
 									<div class="row">
 										<div class="col-md-12 col-lg-4">
 											<ul class="course-features">
-												<li><i class="ti-book"></i> <span class="label">Lectures</span> <span
-														class="value">8</span></li>
-												<li><i class="ti-help-alt"></i> <span class="label">Quizzes</span> <span
-														class="value">1</span></li>
 												<li><i class="ti-time"></i> <span class="label">Duration</span> <span
-														class="value">60 hours</span></li>
+														class="value"><?= $course['duration'] ?> hours</span></li>
 												<li><i class="ti-stats-up"></i> <span class="label">Skill level</span>
-													<span class="value">Beginner</span>
+													<span class="value"><?= $course['skill'] ?></span>
 												</li>
 												<li><i class="ti-smallcap"></i> <span class="label">Language</span>
-													<span class="value">English</span>
-												</li>
-												<li><i class="ti-user"></i> <span class="label">Students</span> <span
-														class="value">32</span></li>
-												<li><i class="ti-check-box"></i> <span class="label">Assessments</span>
-													<span class="value">Yes</span>
+													<span class="value"><?= $course['language'] ?></span>
 												</li>
 											</ul>
 										</div>
 										<div class="col-md-12 col-lg-8">
 											<h5 class="m-b5">Course Description</h5>
-											<p>Lorem Ipsum is simply dummy text of the printing and typesetting
-												industry. Lorem Ipsum has been the industry’s standard dummy text ever
-												since the 1500s, when an unknown printer took a galley of type and
-												scrambled it to make a type specimen book. It has survived not only five
-												centuries, but also the leap into electronic typesetting, remaining
-												essentially unchanged.</p>
+											<p><?= $course['dsc'] ?>.</p>
 											<h5 class="m-b5">Certification</h5>
-											<p>Lorem Ipsum is simply dummy text of the printing and typesetting
-												industry. Lorem Ipsum has been the industry’s standard dummy text ever
-												since the 1500s, when an unknown printer took a galley of type and
-												scrambled it to make a type specimen book. It has survived not only five
-												centuries, but also the leap into electronic typesetting, remaining
-												essentially unchanged.</p>
-											<h5 class="m-b5">Learning Outcomes</h5>
-											<ul class="list-checked primary">
-												<li>Over 37 lectures and 55.5 hours of content!</li>
-												<li>LIVE PROJECT End to End Software Testing Training Included.</li>
-												<li>Learn Software Testing and Automation basics from a professional
-													trainer from your own desk.</li>
-												<li>Information packed practical training starting from basics to
-													advanced testing techniques.</li>
-												<li>Best suitable for beginners to advanced level users and who learn
-													faster when demonstrated.</li>
-												<li>Course content designed by considering current software testing
-													technology and the job market.</li>
-												<li>Practical assignments at the end of every session.</li>
-												<li>Practical learning experience with live project work and examples.cv
-												</li>
-											</ul>
+											<p><?= $course['certification'] ?>.</p>
 										</div>
 									</div>
-								</div>
-								<div class="m-b30" id="curriculum">
-									<h4>Curriculum</h4>
-									<ul class="curriculum-list">
-										<li>
-											<h5>First Level</h5>
-											<ul>
-												<li>
-													<div class="curriculum-list-box">
-														<span>Lesson 1.</span> Introduction to UI Design
-													</div>
-													<span>120 minutes</span>
-												</li>
-												<li>
-													<div class="curriculum-list-box">
-														<span>Lesson 2.</span> User Research and Design
-													</div>
-													<span>60 minutes</span>
-												</li>
-												<li>
-													<div class="curriculum-list-box">
-														<span>Lesson 3.</span> Evaluating User Interfaces Part 1
-													</div>
-													<span>85 minutes</span>
-												</li>
-											</ul>
-										</li>
-										<li>
-											<h5>Second Level</h5>
-											<ul>
-												<li>
-													<div class="curriculum-list-box">
-														<span>Lesson 1.</span> Prototyping and Design
-													</div>
-													<span>110 minutes</span>
-												</li>
-												<li>
-													<div class="curriculum-list-box">
-														<span>Lesson 2.</span> UI Design Capstone
-													</div>
-													<span>120 minutes</span>
-												</li>
-												<li>
-													<div class="curriculum-list-box">
-														<span>Lesson 3.</span> Evaluating User Interfaces Part 2
-													</div>
-													<span>120 minutes</span>
-												</li>
-											</ul>
-										</li>
-										<li>
-											<h5>Final</h5>
-											<ul>
-												<li>
-													<div class="curriculum-list-box">
-														<span>Part 1.</span> Final Test
-													</div>
-													<span>120 minutes</span>
-												</li>
-												<li>
-													<div class="curriculum-list-box">
-														<span>Part 2.</span> Online Test
-													</div>
-													<span>120 minutes</span>
-												</li>
-											</ul>
-										</li>
-									</ul>
 								</div>
 								<div class="" id="instructor">
 									<h4>Instructor</h4>
