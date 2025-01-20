3b93c3b81602c5403b2654e257292ce6213e9bd2
Jad-Bel <jadthegamer06@gmail.com>
2025-01-20 10:19:30 +0100

    FEAT: Refactor teacher dashboard layout by removing unused menu items and updating asset paths

--- pages/add_course.php
+++ pages/add_course.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <!-- Mirrored from educhamp.themetrades.com/demo/admin/add-listing.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 22 Feb 2019 13:09:05 GMT -->
+
 <head>
 
 	<!-- META ============================================= -->
@@ -10,49 +11,50 @@
 	<meta name="keywords" content="" />
 	<meta name="author" content="" />
 	<meta name="robots" content="" />
-	
+
 	<!-- DESCRIPTION -->
 	<meta name="description" content="EduChamp : Education HTML Template" />
-	
+
 	<!-- OG -->
 	<meta property="og:title" content="EduChamp : Education HTML Template" />
 	<meta property="og:description" content="EduChamp : Education HTML Template" />
 	<meta property="og:image" content="" />
 	<meta name="format-detection" content="telephone=no">
-	
+
 	<!-- FAVICONS ICON ============================================= -->
 	<link rel="icon" href="../error-404.html" type="image/x-icon" />
 	<link rel="shortcut icon" type="image/x-icon" href="../assets/images/favicon.png" />
-	
+
 	<!-- PAGE TITLE HERE ============================================= -->
 	<title>EduChamp : Education HTML Template </title>
-	
+
 	<!-- MOBILE SPECIFIC ============================================= -->
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	
+
 	<!--[if lt IE 9]>
 	<script src="assets/js/html5shiv.min.js"></script>
 	<script src="assets/js/respond.min.js"></script>
 	<![endif]-->
-	
+
 	<!-- All PLUGINS CSS ============================================= -->
 	<link rel="stylesheet" type="text/css" href="../assets/assets/css/assets.css">
 	<link rel="stylesheet" type="text/css" href="../assets/assets/vendors/calendar/fullcalendar.css">
-	
+
 	<!-- TYPOGRAPHY ============================================= -->
 	<link rel="stylesheet" type="text/css" href="../assets/assets/css/typography.css">
-	
+
 	<!-- SHORTCODES ============================================= -->
 	<link rel="stylesheet" type="text/css" href="../assets/assets/css/shortcodes/shortcodes.css">
-	
+
 	<!-- STYLESHEETS ============================================= -->
 	<link rel="stylesheet" type="text/css" href="../assets/assets/css/style.css">
 	<link rel="stylesheet" type="text/css" href="../assets/assets/css/dashboard.css">
 	<link class="skin" rel="stylesheet" type="text/css" href="../assets/assets/css/color/color-1.css">
-	
+
 </head>
+
 <body class="ttr-opened-sidebar ttr-pinned-sidebar">
-	
+
 	<!-- header start -->
 	<header class="ttr-header">
 		<div class="ttr-header-wrapper">
@@ -126,16 +128,16 @@
 					<li>
 						<a href="index.html" class="ttr-material-button">
 							<span class="ttr-icon"><i class="ti-home"></i></span>
-		                	<span class="ttr-label">Dashborad</span>
-		                </a>
-		            </li>
+							<span class="ttr-label">Dashborad</span>
+						</a>
+					</li>
 					<li>
 						<a href="add-listing.html" class="ttr-material-button">
 							<span class="ttr-icon"><i class="ti-layout-accordion-list"></i></span>
-		                	<span class="ttr-label">Add listing</span>
-		                </a>
-		            </li>
-		            <li class="ttr-seperate"></li>
+							<span class="ttr-label">Add listing</span>
+						</a>
+					</li>
+					<li class="ttr-seperate"></li>
 				</ul>
 				<!-- sidebar menu end -->
 			</nav>
@@ -153,106 +155,132 @@
 					<li><a href="#"><i class="fa fa-home"></i>Home</a></li>
 					<li>Add listing</li>
 				</ul>
-			</div>	
+			</div>
 			<div class="row">
 				<!-- Your Profile Views Chart -->
 				<div class="col-lg-12 m-b30">
 					<div class="widget-box">
 						<div class="wc-title">
-							<h4>Add listing</h4>
+							<h4>Add Listing</h4>
 						</div>
 						<div class="widget-inner">
 							<form class="edit-profile m-b30">
 								<div class="row">
 									<div class="col-12">
 										<div class="ml-auto">
-											<h3>1. Basic info</h3>
+											<h3>1. Basic Info</h3>
 										</div>
 									</div>
 									<div class="form-group col-6">
-										<label class="col-form-label">Course title</label>
+										<label class="col-form-label">Course Title</label>
 										<div>
-											<input class="form-control" type="text" value="">
+											<input class="form-control" type="text" name="title" value="">
 										</div>
 									</div>
 									<div class="form-group col-6">
-										<label class="col-form-label">Course title</label>
+										<label class="col-form-label">Course Banner</label>
 										<div>
-											<input class="form-control" type="text" value="">
+											<input class="form-control" type="text" name="course_bnr" value="">
 										</div>
 									</div>
 									<div class="form-group col-6">
-										<label class="col-form-label">Course start</label>
+										<label class="col-form-label">Teacher ID</label>
 										<div>
-											<input class="form-control" type="text" value="">
+											<input class="form-control" type="text" name="teacher_id" value="">
 										</div>
 									</div>
 									<div class="form-group col-6">
-										<label class="col-form-label">Course expire</label>
+										<label class="col-form-label">Category ID</label>
 										<div>
-											<input class="form-control" type="text" value="">
+											<input class="form-control" type="text" name="category_id" value="">
 										</div>
 									</div>
 									<div class="form-group col-6">
-										<label class="col-form-label">Teacher name</label>
+										<label class="col-form-label">Skill Level</label>
 										<div>
-											<input class="form-control" type="text" value="">
+											<select class="form-control" name="skill_level">
+												<option value="beginner">Beginner</option>
+												<option value="intermediate">Intermediate</option>
+												<option value="advanced">Advanced</option>
+											</select>
+										</div>
+									</div>
+									<div class="form-group col-6">
+										<label class="col-form-label">Language</label>
+										<div>
+											<input class="form-control" type="text" name="language" value="">
+										</div>
+									</div>
+									<div class="form-group col-6">
+										<label class="col-form-label">Duration</label>
+										<div>
+											<input class="form-control" type="time" name="duration" value="">
+										</div>
+									</div>
+									<div class="form-group col-6">
+										<label class="col-form-label">Type</label>
+										<div>
+											<select class="form-control" name="type">
+												<option value="Video">Video</option>
+												<option value="document">Document</option>
+											</select>
+										</div>
+									</div>
+									<div class="form-group col-6">
+										<label class="col-form-label">Certification</label>
+										<div>
+											<input class="form-control" type="text" name="certification" value="">
 										</div>
 									</div>
 									<div class="seperator"></div>
-									
+
 									<div class="col-12 m-t20">
 										<div class="ml-auto m-b5">
 											<h3>2. Description</h3>
 										</div>
 									</div>
 									<div class="form-group col-12">
-										<label class="col-form-label">Course description</label>
+										<label class="col-form-label">Course Description</label>
 										<div>
-											<textarea class="form-control"> </textarea>
+											<textarea class="form-control" name="description"></textarea>
+										</div>
+									</div>
+									<div class="form-group col-12">
+										<label class="col-form-label">Content</label>
+										<div>
+											<textarea class="form-control" name="content"></textarea>
+										</div>
+									</div>
+									<div class="form-group col-12">
+										<label class="col-form-label">Document Link</label>
+										<div>
+											<input class="form-control" type="text" name="document_link" value="">
+										</div>
+									</div>
+									<div class="form-group col-12">
+										<label class="col-form-label">Video Link</label>
+										<div>
+											<input class="form-control" type="text" name="video_link" value="">
 										</div>
 									</div>
 									<div class="col-12 m-t20">
 										<div class="ml-auto">
-											<h3 class="m-form__section">3. Add Item</h3>
+											<h3 class="m-form__section">3. Additional Info</h3>
+										</div>
+									</div>
+									<div class="form-group col-6">
+										<label class="col-form-label">Created At</label>
+										<div>
+											<input class="form-control" type="timestamp" name="created_at" value="">
+										</div>
+									</div>
+									<div class="form-group col-6">
+										<label class="col-form-label">Updated At</label>
+										<div>
+											<input class="form-control" type="timestamp" name="updated_at" value="">
 										</div>
 									</div>
 									<div class="col-12">
-										<table id="item-add" style="width:100%;">
-											<tr class="list-item">
-												<td>
-													<div class="row">
-														<div class="col-md-4">
-															<label class="col-form-label">Course Name</label>
-															<div>
-																<input class="form-control" type="text" value="">
-															</div>
-														</div>
-														<div class="col-md-3">
-															<label class="col-form-label">Course Category</label>
-															<div>
-																<input class="form-control" type="text" value="">
-															</div>
-														</div>
-														<div class="col-md-3">
-															<label class="col-form-label">Course Category</label>
-															<div>
-																<input class="form-control" type="text" value="">
-															</div>
-														</div>
-														<div class="col-md-2">
-															<label class="col-form-label">Close</label>
-															<div class="form-group">
-																<a class="delete" href="#"><i class="fa fa-close"></i></a>
-															</div>
-														</div>
-													</div>
-												</td>
-											</tr>
-										</table>
-									</div>
-									<div class="col-12">
-										<button type="button" class="btn-secondry add-item m-r5"><i class="fa fa-fw fa-plus-circle"></i>Add Item</button>
 										<button type="reset" class="btn">Save changes</button>
 									</div>
 								</div>
@@ -266,43 +294,44 @@
 	</main>
 	<div class="ttr-overlay"></div>
 
-<!-- External JavaScripts -->
-<script src="../assets/assets/js/jquery.min.js"></script>
-<script src="../assets/assets/vendors/bootstrap/js/popper.min.js"></script>
-<script src="../assets/assets/vendors/bootstrap/js/bootstrap.min.js"></script>
-<script src="../assets/assets/vendors/bootstrap-select/bootstrap-select.min.js"></script>
-<script src="../assets/assets/vendors/bootstrap-touchspin/jquery.bootstrap-touchspin.js"></script>
-<script src="../assets/assets/vendors/magnific-popup/magnific-popup.js"></script>
-<script src="../assets/assets/vendors/counter/waypoints-min.js"></script>
-<script src="../assets/assets/vendors/counter/counterup.min.js"></script>
-<script src="../assets/assets/vendors/imagesloaded/imagesloaded.js"></script>
-<script src="../assets/assets/vendors/masonry/masonry.js"></script>
-<script src="../assets/assets/vendors/masonry/filter.js"></script>
-<script src="../assets/assets/vendors/owl-carousel/owl.carousel.js"></script>
-<script src='../assets/assets/vendors/scroll/scrollbar.min.js'></script>
-<script src="../assets/assets/js/functions.js"></script>
-<script src="../assets/assets/vendors/chart/chart.min.js"></script>
-<script src="../assets/assets/js/admin.js"></script>
-<script src='../assets/assets/vendors/switcher/switcher.js'></script>
-<script>
-// Pricing add
-	function newMenuItem() {
-		var newElem = $('tr.list-item').first().clone();
-		newElem.find('input').val('');
-		newElem.appendTo('table#item-add');
-	}
-	if ($("table#item-add").is('*')) {
-		$('.add-item').on('click', function (e) {
-			e.preventDefault();
-			newMenuItem();
-		});
-		$(document).on("click", "#item-add .delete", function (e) {
-			e.preventDefault();
-			$(this).parent().parent().parent().parent().remove();
-		});
-	}
-</script>
+	<!-- External JavaScripts -->
+	<script src="../assets/assets/js/jquery.min.js"></script>
+	<script src="../assets/assets/vendors/bootstrap/js/popper.min.js"></script>
+	<script src="../assets/assets/vendors/bootstrap/js/bootstrap.min.js"></script>
+	<script src="../assets/assets/vendors/bootstrap-select/bootstrap-select.min.js"></script>
+	<script src="../assets/assets/vendors/bootstrap-touchspin/jquery.bootstrap-touchspin.js"></script>
+	<script src="../assets/assets/vendors/magnific-popup/magnific-popup.js"></script>
+	<script src="../assets/assets/vendors/counter/waypoints-min.js"></script>
+	<script src="../assets/assets/vendors/counter/counterup.min.js"></script>
+	<script src="../assets/assets/vendors/imagesloaded/imagesloaded.js"></script>
+	<script src="../assets/assets/vendors/masonry/masonry.js"></script>
+	<script src="../assets/assets/vendors/masonry/filter.js"></script>
+	<script src="../assets/assets/vendors/owl-carousel/owl.carousel.js"></script>
+	<script src='../assets/assets/vendors/scroll/scrollbar.min.js'></script>
+	<script src="../assets/assets/js/functions.js"></script>
+	<script src="../assets/assets/vendors/chart/chart.min.js"></script>
+	<script src="../assets/assets/js/admin.js"></script>
+	<script src='../assets/assets/vendors/switcher/switcher.js'></script>
+	<script>
+		// Pricing add
+		function newMenuItem() {
+			var newElem = $('tr.list-item').first().clone();
+			newElem.find('input').val('');
+			newElem.appendTo('table#item-add');
+		}
+		if ($("table#item-add").is('*')) {
+			$('.add-item').on('click', function(e) {
+				e.preventDefault();
+				newMenuItem();
+			});
+			$(document).on("click", "#item-add .delete", function(e) {
+				e.preventDefault();
+				$(this).parent().parent().parent().parent().remove();
+			});
+		}
+	</script>
 </body>
 
 <!-- Mirrored from educhamp.themetrades.com/demo/admin/add-listing.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 22 Feb 2019 13:09:05 GMT -->
+
 </html>
