aa552934bc8ddeb5962c1f007198bbb1ee878d65
Jad-Bel <jadthegamer06@gmail.com>
2025-01-24 11:35:58 +0100

    MODIFY: Update require statements and namespace usage in courses.php for improved organization and clarity

--- pages/courses.php
+++ pages/courses.php
@@ -1,10 +1,15 @@
 <?php
-require_once '../config/database.php';
-require_once '../classes/admin/course.php';
-require_once '../classes/admin/courseService.php';
-require_once '../classes/user.php';
-require_once '../classes/admin/student.php';
-require_once '../classes/admin/category.php';
+require_once '../classes/database.php';
+require_once '../classes/App/Course/course.php';
+require_once '../classes/App/Course/courseService.php';
+require_once '../classes/App/User/user.php';
+require_once '../classes/App/User/student.php';
+require_once '../classes/App/category.php';
+
+use App\Course\ConcreteCourse;
+use App\CourseService\CourseService;
+use App\Categories\Category;
+
 
 $courseModel = new ConcreteCourse(NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 $categoryModel = new Category();
